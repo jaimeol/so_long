f6271b0cc915bc3ff26961702a2c6e58b3c1eeb0
Jaime Olivares Cepeda <jolivare@c2r9s6.42madrid.com>
2024-03-09 12:29:34 +0100

    Hoal

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: jolivare <jolivare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 10:29:55 by jolivare          #+#    #+#             */
-/*   Updated: 2024/03/08 11:19:42 by jolivare         ###   ########.fr       */
+/*   Updated: 2024/03/09 12:28:42 by jolivare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@ int main(int argc, char *argv[])
 {
 	t_window window;
 	window.mlx = mlx_init();
+	if (argc != 2)
+	{
+		ft_printf("Error: Número de argumentos inválido\n");
+		exit (1);
+	}
 	if (!window.mlx)
 	{
 		ft_printf("Fallo\n");
@@ -25,12 +30,12 @@ int main(int argc, char *argv[])
     t_game *game;
     char *map_file = argv[1];
     char *map_content = read_map(map_file, &game);
-	checker(game);
 	(*game).map = create_map(map_file, &game);
+	checker(game);
+	printf("%d\n", (*game).x_large);
+	printf("%d\n", (*game).y_large);
 	window.win = mlx_new_window(window.mlx, (*game).x_large, (*game).y_large, "juego");
 	mlx_loop(window.mlx);
-	free(window.mlx);
-	free(map_content);
-	argc = 0;
+	free (map_content);
 	exit (0);
 }
